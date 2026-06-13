71771f207327ccefc44c91683b086baf260b63c7
mercy <mihretyirga7@gmail.com>
2026-06-13 15:56:23 -0300

    day 2 task 2 completed

---
+++ Day2/GroupAnagram.go
@@ -0,0 +1,26 @@
+package main
+
+import "sort"
+
+func groupAnagrams(strs []string) [][]string {
+	groups := make(map[string][]string)
+
+	for _, str := range strs {
+		chars := []byte(str)
+		sort.Slice(chars, func(i, j int) bool {
+			return chars[i] < chars[j]
+		})
+
+		key := string(chars)
+
+		groups[key] = append(groups[key], str)
+	}
+
+	result := [][]string{}
+
+	for _, group := range groups {
+		result = append(result, group)
+	}
+
+	return result
+}
