71e922c85c71f06dde9ff0d51fbb9b3cd8a34aa8
mercy <mihretyirga7@gmail.com>
2026-06-13 15:54:39 -0300

    day 2 task 1 completed

---
+++ Day2/TwoSum.go
@@ -0,0 +1,32 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+//   Complexity :  Brute Force (O(n²))
+
+func twoSumOptimal(nums []int, target int) []int {
+	seen := make(map[int]int)
+
+	for i, num := range nums {
+		complement := target - num
+
+		if idx, exists := seen[complement]; exists {
+			return []int{idx, i}
+		}
+
+		seen[num] = i
+	}
+
+	return nil
+}
+
+// optimal solution using HashMap (O(n))
